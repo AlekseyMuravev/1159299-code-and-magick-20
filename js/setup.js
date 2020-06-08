ab9df9057cdae0c9db69254be5fc04f55ac1a0a8
Алексей Муравьёв <chacnoriss@gmail.com>
2020-06-08 14:54:50 +0300

    нас орда

--- js/setup.js
+++ js/setup.js
@@ -1,49 +1,51 @@
 'use strict';
 
+var NAME_PERSONE = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var SURNAME_PERSONE = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+
 var setup = document.querySelector('.setup');
 setup.classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
 
-// массив с персонажами
-var arrayPersones = function () {
-  var persones = [];
+var persones = [];
 
+// массив всех персонажамей
+var createPersones = function () {
   for (var i = 0; i < 4; i++) {
-    persones.push(arrayPersone(i));
+    persones.push(createPersone());
   }
-  return persones;
 };
 
+// получаем случайное число
 var getRandomArbitrary = function (min, max) {
   return Math.random() * (max - min) + min;
 };
 
-var namePersone = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var surnamePersone = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Топольницкая', ' Нионго', ' Ирвинг'];
-var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
-
 // создание одного персонажа
-var arrayPersone = function () {
+var createPersone = function () {
   var persone = {};
-  persone.name = (namePersone[Math.round(getRandomArbitrary(0, namePersone.length - 1))] + surnamePersone[Math.round(getRandomArbitrary(1, surnamePersone.length - 1))]);
-  persone.coatColor = coatColor[Math.round(getRandomArbitrary(0, coatColor.length - 1))];
-  persone.eyesColor = eyesColor[Math.round(getRandomArbitrary(0, eyesColor.length - 1))];
+  persone.name = (NAME_PERSONE[Math.round(getRandomArbitrary(0, NAME_PERSONE.length - 1))] + ' ' + SURNAME_PERSONE[Math.round(getRandomArbitrary(0, SURNAME_PERSONE.length - 1))]);
+  persone.coatColor = COAT_COLOR[Math.round(getRandomArbitrary(0, COAT_COLOR.length - 1))];
+  persone.eyesColor = EYES_COLOR[Math.round(getRandomArbitrary(0, EYES_COLOR.length - 1))];
   return persone;
 };
 
-// заполнение персонажей данными
-var fillingPersones = function () {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = arrayPersones()[i].name;
-  wizardElement.querySelector('.wizard-coat').style.fill = arrayPersones()[i].coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = arrayPersones()[i].eyesColor;
-  setupSimilarList.appendChild(wizardElement);
-};
+createPersones();
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var setupSimilarList = document.querySelector('.setup-similar-list');
 
-for (var i = 0; i < 4; i++) {
-  fillingPersones(i);
+// заполнение персонажей данными
+var fillingPersones = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  setupSimilarList.appendChild(wizardElement);
+};
+
+for (var i = 0; i < persones.length; i++) {
+  fillingPersones(persones[i]);
 }
